87c690a6e62154926e12fa4ebac722aaf39ffb96
Marekepf <125371571+Marekepf@users.noreply.github.com>
2023-04-04 11:59:14 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,25 +1,57 @@
-from flask import Flask,request
-import os
+from flask import Flask, request
 import openai
+import os
 
 app = Flask(__name__)
 
-openai.api_key = os.environ.get('OPENAI_KEY')
+# Set the OpenAI API key
+openai.api_key = os.environ.get("OPENAI_KEY")
 
-
-@app.route('/')
-def index():
-    return "<h1>Hello, World!</h1>"
-
-@app.route('/chatgpt')
+@app.route('/chatgpt', methods=['GET'])
 def chatgpt():
-    args = request.args
-    message =args.get("message")
-    print(message)
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": message}]
+    # Get the message from the request query parameters
+    message = request.args.get('message')
+
+    # Generate a response using the OpenAI GPT-3 API
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
     )
-    return completion['choices'][0]['message']['content']
+
+    # Extract the response text from the OpenAI API response
+    response_text = response.choices[0].text.strip()
+
+    # Return the response text
+    return response_text
+
+@app.route('/codegen', methods=['POST'])
+def codegen():
+    # Get the language and content from the request JSON data
+    language = request.json['language']
+    content = request.json['content']
+
+    # Generate code using the OpenAI GPT-3 API
+    prompt = f"Generate {language} code for the following task:\n\n{content}\n\n"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    # Extract the response text from the OpenAI API response
+    response_text = response.choices[0].text.strip()
+
+    # Return the generated code
+    return response_text
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
