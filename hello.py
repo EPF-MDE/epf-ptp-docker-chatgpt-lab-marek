1f4f474bf16886deabecc51609589dd8259a584a
Marekepf <125371571+Marekepf@users.noreply.github.com>
2023-04-04 13:01:07 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 import os
 import openai
 
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 openai.api_key = os.environ.get('OPENAI_KEY')
 
+
 @app.route('/')
 def index():
     return "<h1>Hello, World!</h1>"
 
-
+#we use app.route to define the route of the API which means the URL
 @app.route('/chatgpt')
 def chatgpt():
     args = request.args
@@ -30,12 +31,11 @@ def chatgpt():
     return response
 
 
-@app.route('/create_code', methods=['POST'])#we use the POST method because we send data to the API
+@app.route('/create_code', methods=['POST']) #we use the POST method because we send data to the API
 def create_code():
-    data = request.json
     #we set the language and the content
-    language = data['language']
-    content = data['content']
+    language = request.json['language']
+    content = request.json['content']
     #we create the prompt
     completion = openai.Completion.create(
         #we use the davinci-codex model
@@ -49,3 +49,6 @@ def create_code():
     #we return the response from the API 
     response = completion.choices[0].text.strip() #the completion is a list of choices, we take the first one
     return response
+
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
