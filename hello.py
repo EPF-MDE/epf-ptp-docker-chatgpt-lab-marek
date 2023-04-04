5f15f5cab949f9e271cca856a8b1e015da01efa4
Marekepf <125371571+Marekepf@users.noreply.github.com>
2023-04-04 14:06:07 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask,request
 import os
 import openai
 
@@ -11,44 +11,39 @@ openai.api_key = os.environ.get('OPENAI_KEY')
 def index():
     return "<h1>Hello, World!</h1>"
 
-#we use app.route to define the route of the API which means the URL
 @app.route('/chatgpt')
 def chatgpt():
     args = request.args
-    message = args.get("message")
+    message =args.get("message")
     print(message)
     completion = openai.ChatCompletion.create(
-        model="text-davinci-002",
-        prompt=f"{message}\nCode:",
-        temperature=0.7,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        frequency_penalty=0,
-        presence_penalty=0
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}]
     )
-    response = completion.choices[0].text.strip()
-    return response
+    return completion['choices'][0]['message']['content']
 
-
-@app.route('/create_code', methods=['POST']) #we use the POST method because we send data to the API
-def create_code():
-    #we set the language and the content
+@app.route('/codegen', methods=['POST'])
+def codegen():
+    # Get the language and content from the request JSON data
     language = request.json['language']
     content = request.json['content']
-    #we create the prompt
-    completion = openai.Completion.create(
-        #we use the davinci-codex model
-        engine="davinci-codex",
-        prompt=f"Generate {language} code:\n{content}\n",
+
+    # Generate code using the OpenAI GPT-3 API
+    prompt = f"Generate {language} code for the following task:\n\n{content}\n\n"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.7,
     )
-    #we return the response from the API 
-    response = completion.choices[0].text.strip() #the completion is a list of choices, we take the first one
-    return response
 
-    if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # Extract the response text from the OpenAI API response
+    response_text = response.choices[0].text.strip()
+
+    # Return the generated code
+    return response_text
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
